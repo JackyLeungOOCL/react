8f091f8de6ed89631b9578f54c5e614cbf289f62
jacky.leung <jacky.leung@oocl.com>
2018-12-05 11:16:26 +0800

    [LEUNGJA] refactor: move all logics to redux

--- src/Components/CounterGroup.js
+++ src/Components/CounterGroup.js
@@ -4,15 +4,22 @@ import CounterGenerator from './CounterGenerator'
 import { connect } from "react-redux";
 
 class CounterGroup extends Component {
-  state = {
-    // sum : 0,
-    counters : new Array(this.props.total).fill(0).map(() => {
-      return {
-        id : new Date().getTime() + Math.random(),
-        number : 0
-      }
-    })
-  };
+  constructor(props) {
+    super(props);
+    this.props.dispatch({
+      type : "GENERATE_COUNTERS",
+      payload : this.props.total
+    });
+  }
+  // state = {
+  //   // sum : 0,
+  //   counters : new Array(this.props.total).fill(0).map(() => {
+  //     return {
+  //       id : new Date().getTime() + Math.random(),
+  //       number : 0
+  //     }
+  //   })
+  // };
 
 
   
@@ -26,56 +33,67 @@ class CounterGroup extends Component {
   }
 
   setNum = (num) => {
-    this.setState({
-      counters : new Array(num).fill(0).map(() => {
-        return {
-          id : new Date().getTime() + Math.random(),
-          number : 0
-        }
-      })
-    })
+    // this.setState({
+    //   counters : new Array(num).fill(0).map(() => {
+    //     return {
+    //       id : new Date().getTime() + Math.random(),
+    //       number : 0
+    //     }
+    //   })
+    // })
     this.props.dispatch({
-      type: "SUM",
-      payload: -this.props.sum
-    })
+      type : "GENERATE_COUNTERS",
+      payload : num
+    });
+    // this.props.dispatch({
+    //   type: "SUM",
+    //   payload: -(this.props.sum)
+    // });
     
   }
 
   counterAdd = (id) => {
-    let newCounter = this.state.counters.map(counter => {
-      if (counter.id === id) {
-        return {
-          id : id,
-          number : counter.number + 1
-        }
-      }else {
-        return counter
-      }
-    })
-    this.setState({counters : newCounter});
+    // let newCounter = this.state.counters.map(counter => {
+    //   if (counter.id === id) {
+    //     return {
+    //       id : id,
+    //       number : counter.number + 1
+    //     }
+    //   }else {
+    //     return counter
+    //   }
+    // })
+    this.props.dispatch({
+      type : "COUNTER_INCREASE",
+      payload : id
+    });
+    // this.setState({counters : newCounter});
     this.updateSum(1);
-    console.log(this.state.counters)
   }
 
   counterMinus = (id) => {
-    let newCounter = this.state.counters.map(counter => {
-      if (counter.id === id) {
-        return {
-          id : id,
-          number : counter.number - 1
-        }
-      }else {
-        return counter
-      }
-    })
-    this.setState({counters : newCounter});
+    // let newCounter = this.state.counters.map(counter => {
+    //   if (counter.id === id) {
+    //     return {
+    //       id : id,
+    //       number : counter.number - 1
+    //     }
+    //   }else {
+    //     return counter
+    //   }
+    // })
+    this.props.dispatch({
+      type : "COUNTER_DECREASE",
+      payload : id
+    });
+    // this.setState({counters : newCounter});
     this.updateSum(-1);
   }
 
   render() {
     return (
       <div>
-        <div>{this.state.counters.map(counter => <Counter id={counter.id} count={counter.number} onIncreased={this.counterAdd} onDecreased={this.counterMinus}/>)}</div>
+        <div>{this.props.counters.map(counter => <Counter id={counter.id} count={counter.number} onIncreased={this.counterAdd} onDecreased={this.counterMinus}/>)}</div>
         <div>
           <CounterGenerator applyChange={this.setNum} />
         </div>
@@ -86,9 +104,8 @@ class CounterGroup extends Component {
 }
 
 const mapStateToProps = state => ({
-  sum : state.sum
+  sum : state.sum,
+  counters : state.counters
 });
-
-connect(mapStateToProps)(CounterGroup)
 
 export default connect(mapStateToProps)(CounterGroup);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,14 +1,59 @@
 const initialState = {
-    sum : 0
+    sum : 0,
+    counters : []
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
 
-  case "SUM":
-    return { sum : state.sum + payload }
+    case "SUM":
+      return {
+        ...state,
+        sum : state.sum + payload
+      };
+    
+    case "GENERATE_COUNTERS":
+      return {
+        sum : 0,
+        counters : new Array(payload).fill(0).map(() => {
+          return {
+            id : new Date().getTime() + Math.random(),
+            number : 0
+          }
+        })
+      };
+    
+    case "COUNTER_INCREASE":
+      return {
+        ...state,
+        counters : state.counters.map(counter => {
+          if (counter.id === payload) {
+            return {
+              id : payload,
+              number : counter.number + 1
+            }
+          }else {
+            return counter
+          }
+        })
+      };
+    
+    case "COUNTER_DECREASE":
+      return {
+        ...state,
+        counters : state.counters.map(counter => {
+          if (counter.id === payload) {
+            return {
+              id : payload,
+              number : counter.number - 1
+            }
+          }else {
+            return counter
+          }
+        })
+      };
 
-  default:
-    return state
+    default:
+      return state
   }
 };
